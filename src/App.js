05b1e6ca0c7a0918d915ae73dc0744ed22a3571b
CriRipa02 <cri.ripa.cr@gmail.com>
2023-06-16 15:49:30 +0200

    emits

--- src/App.js
+++ src/App.js
@@ -4,31 +4,83 @@ import Models from "./sections/models";
 import Colors from "./sections/colors";
 import Accessories from "./sections/accessories";
 import Summary from "./sections/summary";
+import { useState, useContext, createContext } from "react";
+
+const UserContext = createContext();
 
 function App() {
+  const [page, setPage] = useState("models");
+  const [total, setTotal] = useState(0);
+
+  const url = [
+    { name: "models", component: Models },
+    { name: "colors", component: Colors },
+    { name: "accessories", component: Accessories },
+    { name: "summary", component: Summary }
+  ];
+
+  const updateTotal = (newTotal) => {
+    setTotal(newTotal);
+  }
+
   return (
     <div className="App">
-      <header className="main-header">
-        <h1>Product Builder</h1>
-        <nav className="cd-builder-main-nav">
+      <div className="cd-product-builder">
+        <header className="main-header">
+          <h1>Product Builder</h1>
           <Router>
-            <ul className="navbar-nav mr-auto">
-              <li><Link to={'/models'} className="nav-link">Models</Link></li>
-              <li><Link to={'/colors'} className="nav-link">Colors</Link></li>
-              <li><Link to={'/accessories'} className="nav-link">Accessories</Link></li>
-              <li><Link to={'/summary'} className="nav-link">Summary</Link></li>
-            </ul>
-            <Routes>
-              <Route index element={<Models />} />
-              <Route path='/models' element={<Models />} />
-              <Route path='/colors' element={<Colors />} />
-              <Route path='/accessories' element={<Accessories />} />
-              <Route path='/summary' element={<Summary />} />
-            </Routes>
+            <nav className="cd-builder-main-nav">
+              <ul className="navbar-nav mr-auto">
+                {url.map(ele =>
+                  <li className={page === ele.name ? "active" : ""} onClick={() => setPage(ele.name)} key={ele.name}>
+                    <Link to={"/" + ele.name} className="nav-link">{ele.name}</Link>
+                  </li>
+                )}
+              </ul>
+            </nav>
+            
+            <UserContext.Provider value={total}>
+              <Routes>
+                <Route index element={<Models />} />
+                {url.map(ele =>
+                  <Route path={"/" + ele.name} element={<ele.component total={total} update={updateTotal} />} key={ele.name} />
+                )}
+              </Routes>
+            </UserContext.Provider>
           </Router>
-        </nav>
-      </header>
-    </div >
+        </header>
+        <footer className="cd-builder-footer step-1 disabled">
+          <div className="selected-product">
+            <img src="img/product01_col01.jpg" alt="Product preview" />
+            <div className="tot-price">
+              <span>Total</span>
+              <span className="total">$<b>{total}</b></span>
+            </div>
+          </div>
+          <nav className="cd-builder-secondary-nav">
+            <ul>
+              <li className="next nav-item">
+                <ul>
+                  <li className="visible"><a href="#0">Colors</a></li>
+                  <li className=""><a href="#0">Accessories</a></li>
+                  <li className=""><a href="#0">Summary</a></li>
+                  <li className="buy"><a href="#0">Buy Now</a></li>
+                </ul>
+              </li>
+              <li className="prev nav-item">
+                <ul>
+                  <li className="visible"><a href="#0">Models</a></li>
+                  <li className=""><a href="#0">Models</a></li>
+                  <li className=""><a href="#0">Colors</a></li>
+                  <li className=""><a href="#0">Accessories</a></li>
+                </ul>
+              </li>
+            </ul>
+          </nav>
+          <span className="alert">Please, select a model first!</span>
+        </footer>
+      </div >
+    </div>
   );
 }
 
--- src/sections/models.js
+++ src/sections/models.js
@@ -1,24 +1,50 @@
 import React from "react";
+import { useState, useContext, createContext, useEffect } from "react";
 
 import model1 from "../assets/models/product01.jpg";
 import model2 from "../assets/models/product02.jpg";
 
-export default function Models() {
+export default function Models(props) {
+    const [model, setModel] = useState({});
+    let [total, setTotal] = useState(Number(props.total));
+
+    const models = [
+        { name: "BMW i3", img: model1, value: "product-01", price: "42400", textPrice: "from $42.400" },
+        { name: "BMW i8", img: model2, value: "product-02", price: "140700", textPrice: "from $140.700" }
+    ]
+
+    const valorizeModel = (data) => {
+        if (!model.name) {
+            setModel(data);
+            //setTotal(total + Number(data.price));
+            props.update(total + Number(data.price))
+        } else {
+            if (model.value === data.value) {
+                setModel({});
+                setTotal(total - Number(data.price));
+                props.update(total - Number(data.price))
+            } else {
+                setModel(data);
+                setTotal(Number(props.total) + Number(data.price));
+                props.update(Number(props.total) + Number(data.price))
+            }
+        }
+    }
+
     return (
-        <section className="cd-step-content">
+        <section className="cd-builder-steps cd-step-content">
             <ul className="models-list options-list cd-col-2">
-                <li className="js-option js-radio" data-price="42400" data-model="product-01">
-                    <span className="name">BMW i3</span>
-                    <img src={model1} alt="BMW i3" />
-                    <span className="price">from $42.400</span>
-                    <div className="radio"></div>
-                </li>
-                <li className="js-option js-radio" data-price="140700" data-model="product-02">
-                    <span className="name">BMW i8</span>
-                    <img src={model2} alt="BMW i8" />
-                    <span className="price">from $140.700</span>
-                    <div className="radio"></div>
-                </li>
+                {models.map(ele =>
+                    <li className={`js-option js-radio ${ele.value === model.value ? "selected loaded" : ""}`}
+                        data-price={ele.price} data-model={ele.value}
+                        onClick={() => valorizeModel(ele)} key={ele.name}
+                    >
+                        <span className="name">{ele.name}</span>
+                        <img src={ele.img} alt={ele.name} />
+                        <span className="price">{ele.textPrice}</span>
+                        <div className="radio" />
+                    </li>
+                )}
             </ul>
         </section>
     );
